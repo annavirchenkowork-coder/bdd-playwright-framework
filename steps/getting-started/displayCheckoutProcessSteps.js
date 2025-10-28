0b7f6a5ec0e0c562765ed0070aa622921edb538e
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-10-28 09:08:53 -0400

    feat: add step definitions skeleton for checkout stepper verification

--- steps/getting-started/displayCheckoutProcessSteps.js
+++ steps/getting-started/displayCheckoutProcessSteps.js
@@ -1,4 +1,22 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { startApplicationPage, page } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { startApplicationPage } from "../../globalPagesSetup.js";
+
+const ACTIVE_BLUE = "rgb(1, 201, 255)";
+
+// ---------- AC1: verify step labels & order ----------
+Then(
+  "The checkout stepper should display the following steps in order:",
+  async function (dataTable) {}
+);
+
+// ---------- AC2/AC3: verify active/inactive state via circle color ----------
+Then(
+  'The "Start Application" step should be active and blue',
+  async function () {}
+);
+
+Then(
+  "The following steps should be inactive and grey:",
+  async function (dataTable) {}
+);
