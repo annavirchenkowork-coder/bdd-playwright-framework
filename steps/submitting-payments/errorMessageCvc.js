6745559a17ed13eff46cbc4a20472fdac2737cfd
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-06 08:30:09 -0500

    chore(sep29): add step definition skeleton for invalid CVC error validation

--- steps/submitting-payments/errorMessageCvc.js
+++ steps/submitting-payments/errorMessageCvc.js
@@ -1,4 +1,21 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { reviewPaymentPage } from "../../globalPagesSetup.js";
+
+When(
+  "User types {string} into the Security Code field",
+  async function (string) {
+    
+  }
+);
+
+Then("The Security Code field error should be visible", async function () {
+  
+});
+
+Then(
+  "The Security Code field error should contain {string}",
+  async function (string) {
+   
+  }
+);
