f6f40d6211ff930f71924b4c8363e60d77755b51
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-06 07:54:53 -0500

    chore(sep25): add step definition skeleton for card number error validation

--- steps/submitting-payments/errorMessageCardNumber.js
+++ steps/submitting-payments/errorMessageCardNumber.js
@@ -1,4 +1,24 @@
 import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { expect } from "@playwright/test";
+import { reviewPaymentPage } from "../../globalPagesSetup.js";
+
+/* small helper: type value into Stripe field */
+async function fillStripeInput(locator, value) {
+  await expect(locator).toBeVisible();
+  await locator.fill(""); // clear any previous value
+  await locator.type(value); // let Stripe auto-format the digits
+}
+
+When(
+  "User types {string} into the Card Number field",
+  async function (string) {}
+);
+
+When("User checks the Terms and Conditions checkbox", async function () {});
+
+Then("The Card Number field error should be visible", async function () {});
+
+Then(
+  "The Card Number field error should contain {string}",
+  async function (string) {}
+);
