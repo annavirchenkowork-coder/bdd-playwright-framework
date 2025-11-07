e45eac9eff70707bf38356967c2df817cd03b027
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-07 08:30:22 -0500

    feat(sep27): implement expiration date field validation steps

--- steps/submitting-payments/errorMessageExpirationDate.js
+++ steps/submitting-payments/errorMessageExpirationDate.js
@@ -1,4 +1,21 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { reviewPaymentPage } from "../../globalPagesSetup.js";
+import { microSettle } from "../../utilities/BrowserUtility.js";
+
+/* ---- Expiration Date input ---- */
+When(
+  "User types {string} into the Expiration Date field",
+  async function (string) {}
+);
+/* ---- Expiration Date error visibility ---- */
+Then("The Expiration Date field error should be visible", async function () {
+  
+});
+/* ---- Expiration Date error text ---- */
+Then(
+  "The Expiration Date field error should contain {string}",
+  async function (string) {
+   
+  }
+);
