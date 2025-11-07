045fe7ece9eae3212f1a9dc8ab61271bd6ed4a49
Anna Virchenko <anna.virchenko.work@gmail.com>
2025-11-07 08:48:24 -0500

    chore(sep23): add step definition skeleton for successful payment flow

--- steps/submitting-payments/makePayment.js
+++ steps/submitting-payments/makePayment.js
@@ -1,4 +1,23 @@
-import { Given, Then, When } from "@cucumber/cucumber";
-import { expect} from "@playwright/test";
-import { reviewPaymentPage, page } from "../../globalPagesSetup.js";
-import { productInfo } from "../../utilities/qa-data-reader.js";
+import { When, Then } from "@cucumber/cucumber";
+import { expect } from "@playwright/test";
+import { reviewPaymentPage } from "../../globalPagesSetup.js";
+import { microSettle } from "../../utilities/BrowserUtility.js";
+
+/* ---- Fill valid payment data (Stripe test card) ---- */
+When("User enters a valid card number", async function () {});
+
+When("User enters a valid expiration date", async function () {});
+
+When("User enters a valid Security Code", async function () {});
+
+When("User enters a valid ZIP code", async function () {});
+
+When("User clicks the Pay button", async function () {});
+
+/* ---- Assertions for success state ---- */
+Then(
+  "The payment confirmation message should be displayed",
+  async function () {}
+);
+
+Then("The stepper should show all steps completed", async function () {});
